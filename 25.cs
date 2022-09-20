31ffb8c77024cb9861772e0198de7e86e619bebb
alilinkin <sail94@mail.ru>
2022-09-20 19:54:42 +0500

    25

---
+++ 25.cs
@@ -0,0 +1,19 @@
+﻿double NaturalDegree(int A, int B)
+{
+    double result = Math.Pow(A, B);
+    return result;
+}
+
+try
+{
+    Console.Write("Введите число A: ");
+    int a = int.Parse(Console.ReadLine());
+    Console.Write("Введите число B: ");
+    int b = int.Parse(Console.ReadLine());
+    double result = NaturalDegree(a, b);
+    Console.WriteLine($"число {a} в натуральной степени {b} = {result}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Входное значение имеет неверный формат. Ошибка: {ex.Message}");
+}
